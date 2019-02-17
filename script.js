433dfcdc315b89c49d9ec3310c20d20156c9b560
Matteo <matteo.delas7@icloud.com>
2019-02-17 17:08:43 +0100

    site v1

---
+++ script.js
@@ -0,0 +1,17 @@
+var nav = document.getElementById('nav');
+var navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+document.getElementById('nav-icon').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleNav();
+});
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
+}
